b2d2733595866ce4613b80f3c67f6e0fdacb9b23
Ali Majidi <amajidi1382@gmail.com>
2024-08-28 17:05:02 +0330

    feat: improve Layout component element positions

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,14 +2,15 @@ import { Outlet } from "react-router-dom"
 
 import Header from "@components/Header/Header"
 import Footer from "@components/Footer/Footer"
+import "./style.scss";
 
 function Layout() {
 	return (
-		<>
+		<div className="layout">
 			<Header />	
 			<Outlet />
 			<Footer />
-		</>
+		</div>
 	)
 }
 
