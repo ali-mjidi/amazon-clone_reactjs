5215958a5b7a588f52b6e1191ac016b36fd22fec
Ali Majidi <amajidi1382@gmail.com>
2024-09-05 15:55:26 +0330

    fix: fix some styles problems

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -33,42 +33,45 @@ function Product() {
         <div className="product">
             <div className="product__visualInformation">
                 <div className="product__imagesWrapper">
-                    <img
-                        className="product__thumbnail"
-                        src={productInfo?.imageLink?.at(
-                            additionalInfo.activeImage
-                        )}
-                        alt={productInfo?.title}
-                    />
-
-                    <div className="product__otherImagesWrapper">
-                        {productInfo?.imageLink?.map((link, index) => (
-                            <img
-                                key={index}
-                                className={`product__otherImage ${
-                                    additionalInfo.activeImage === index &&
-                                    "product__otherImage--active"
-                                }`}
-                                src={link}
-                                alt={productInfo?.title}
-                                onPointerEnter={() => handleChangeImage(index)}
-                            />
-                        ))}
+                    <div className="product__thumbnailWrapper">
+                        <img
+                            className="product__thumbnail"
+                            src={productInfo?.imageLink?.at(
+                                additionalInfo.activeImage
+                            )}
+                            alt={productInfo?.title}
+                        />
+                        <div className="product__otherImagesWrapper">
+                            {productInfo?.imageLink?.map((link, index) => (
+                                <img
+                                    key={index}
+                                    className={`product__otherImage ${
+                                        additionalInfo.activeImage === index &&
+                                        "product__otherImage--active"
+                                    }`}
+                                    src={link}
+                                    alt={productInfo?.title}
+                                    onPointerEnter={() =>
+                                        handleChangeImage(index)
+                                    }
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
-                <p>
+                <div className="product__authorWrapper">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Exercitationem saepe voluptatum quas cum debitis rerum, ad
                     iusto fugiat illo maiores molestiae sequi cumque,
                     repellendus sit expedita adipisci pariatur hic ducimus.
-                </p>
+                </div>
             </div>
             <div className="product__info">
                 <ProductTitle />
 
                 <ProductDescription />
 
-				{category === "book" && <BookProductDetails />}
+                {category === "book" && <BookProductDetails />}
             </div>
             <div className="product__buy"></div>
         </div>
