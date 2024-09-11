50c5a84a8f39169ae4ecfdd7839ca3020b83d934
Ali Majidi <amajidi1382@gmail.com>
2024-09-11 16:17:04 +0330

    style: add comment on BackDrop component

--- src/components/BackDrop/BackDrop.jsx
+++ src/components/BackDrop/BackDrop.jsx
@@ -3,6 +3,11 @@ import { createPortal } from "react-dom";
 import "./style.scss";
 
 function BackDrop({ targetId, ...props }) {
+    /* 
+		This component is the dark thing that appears when 
+		user clicks on the side menu or hover on dropdown menus on the header, 
+		and when the use clicks on it, the menu will close.
+	*/
     return createPortal(
         <div className={`backDrop backDrop--${targetId}`} {...props}></div>,
         document.getElementById(targetId)
